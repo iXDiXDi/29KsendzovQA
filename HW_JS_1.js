cf0db91c9477cdfedd91a64b9fc557ada6b4ba8d
iXDiXDi <79995280+iXDiXDi@users.noreply.github.com>
2022-04-18 14:58:38 +0300

    add minor syntax updates
    Внесены синтаксичекие правки и изменено применение метода toString

--- HW_JS_1.js
+++ HW_JS_1.js
@@ -37,15 +37,15 @@ let item_6_type
 //  19. Присвоить переменной item_6 значение 15
 item_6 = 15
 //  20. Присвоить переменной item_6_type тип переменной item_6
-item_6_type = typeof(item_6)
+item_6_type = typeof item_6
 //  21. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——  
 console.log (`"item_6" == ` + item_6 + " " + `"item_6_type" == ` + item_6_type)
 //  22. Создать переменную item_7 и в ней преобразовать item_6 в String.
-let item_7 = toString(item_6)
+let item_7 = item_6.toString()
 //  23. Создать переменную item_7_type
 let item_7_type
 //  24. Присвоить переменной item_7_type тип переменной item_7
-item_7_type = typeof(item_7)
+item_7_type = typeof item_7
 //  25. Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——  
 console.log (`"item_7" == ` + item_7 + " " + `"item_7_type" == ` + item_7_type)
 //  26. Создать переменную “age_1” и присвоить ей значение 10
@@ -55,7 +55,7 @@ let age_2 = 18
 //  28. Создать переменную “age_3” и присвоить ей значение 60
 let age_3 = 60
 //  29. Создать if в котором будите проверять значение переменной age_1
-if (typeof(age_1) == "number") {
+if (typeof age_1 == "number") {
 //  30. Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is ” + age_1 + “ It’s less then ”
     if (age_1 < age_2) {
         console.log("You don’t have access cause your age is " + age_1 + " It’s less then " + age_2)
